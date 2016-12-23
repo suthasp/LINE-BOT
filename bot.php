5b2f106b52f0eaad8ccb31a66e48141c3b43a53d
Suthas <suthasp@gmail.com>
2016-12-23 22:28:13 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -37,7 +37,6 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($ch, CURLOPT_URL, 'https://th.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$post);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
