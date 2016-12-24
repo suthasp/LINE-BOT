da774fe58f295d1c93bdbe57289cd7e6a4529a19
Suthas <suthasp@gmail.com>
2016-12-24 10:57:48 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello, user'
+				'text' => 'Hello, May I help you?'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
