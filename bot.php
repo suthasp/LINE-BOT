5b055a606a93f719e940fbb658fe4fdaf306d36b
Suthas <suthasp@gmail.com>
2016-12-24 16:22:17 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello, <userId>'
+				'text' => 'สวัสดีปีใหม่ 2017'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
