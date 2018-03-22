a02c2d39cffdeafd45a625bf579289f0b01c876d
Suthas <suthasp@gmail.com>
2018-03-22 10:18:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'สวัสดีปีใหม่ 2017' 
+				'text' => 'สวัสดีปีใหม่ 2018' 
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
